684cd9485796eb566a92d74ec647a55a458a6a20
rsujithra91 <rsujithra91@gmail.com>
2026-03-27 11:22:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import streamlit as st
-st.title("Streamlit App")
-name=st.text_input("Enter your name")
-if st.button("Submit"):
-  st.write("Name is ",name)
+import pandas as pd
+from sklearn.model_selection import train_est_split
+from sklearn.linear_model import LinearRegression
+df=pd.read_csv("data.sv")
+X=df[[HoursStudied']]
+y=df['ExamScore']
+X-Train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+model=LinearRegression()
+model.fit(X_train,y_train)
+st.title("Exam Score Predictor")
+st.write("Enter hours studied to predict the exam score")
+hours=st.number_input("Hours Studied",min_value=0.0,step=0.1)
+if st.button("Predict Score"):
+     ps=model.predict([[hours]])[0]
+     st.success("Pedicted Score:{ps}")
+st.write("sample training data")
+st.dataframe(df)
+     
