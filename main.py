f6f15a335fffd1ffa76d14dafee0c650b41b16a6
rsujithra91 <rsujithra91@gmail.com>
2026-03-27 11:27:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from sklearn.linear_model import LinearRegression
 df=pd.read_csv("data.csv")
 X=df[['HoursStudied']]
 y=df['ExamScore']
-X-Train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
 model=LinearRegression()
 model.fit(X_train,y_train)
 st.title("Exam Score Predictor")
