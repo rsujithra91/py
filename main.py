6c8d9a96dcee17f72f3c871b5c9c927cd3e47711
rsujithra91 <rsujithra91@gmail.com>
2026-03-27 11:24:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from sklearn.model_selection import train_est_split
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 df=pd.read_csv("data.sv")
 X=df[[HoursStudied']]
